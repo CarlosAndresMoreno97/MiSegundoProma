4b709e585c970102f8bd0821e916afbfd201fe33
Carlos Andres Moreno Borja <calitosmore@outlook.com>
2023-08-31 13:53:15 -0500

    Misegundoprograma

---
+++ MiSegundoProma/Program.cs
@@ -0,0 +1,6 @@
+﻿/*Realizar un algoritmo que me pida 3 números por pantalla, 
+con el primer número hallar el cuadrado, con el segundo número hallar la raíz cuadrada 
+y con el tercer número hallar la raíz cúbica. Mostrar en pantalla los 3 resultados aritméticos.*/
+
+public class MiSegundoProma
+
